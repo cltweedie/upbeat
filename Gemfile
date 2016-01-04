9c567c15918e50198368c6596e301773d30540aa
Chris Tweedie <cltweedie@gmail.com>
2016-01-04 15:40:06 +0000

    Create waveform image of sound when created

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'carrierwave'
+gem 'waveform'
 
 group :test do
   gem 'faker'
--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -8,7 +8,8 @@ class SoundsController < ApplicationController
     @sound = Sound.new(sound_params)
     @sound.producer = current_producer
     @sound.save
-    redirect_to root_path
+    Waveform.generate(@sound.file.path, "#{@sound.file.path}.png")
+    redirect_to sound_path(@sound)
   end
 
   def show
