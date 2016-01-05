403767daa5da57a69a53fcd809551f29e6a2fd31
Chris Tweedie <cltweedie@gmail.com>
2016-01-05 14:46:44 +0000

    Generate tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105140754) do
+ActiveRecord::Schema.define(version: 20160105144621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,12 @@ ActiveRecord::Schema.define(version: 20160105140754) do
     t.string   "title"
     t.integer  "instrument_id"
     t.integer  "category_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
