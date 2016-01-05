41ea89f15eea1abd878f6c88489778948ad6aa96
Chris Tweedie <cltweedie@gmail.com>
2016-01-05 12:04:09 +0000

    Add unit tests for GET to sounds

--- spec/controllers/sounds_controller_spec.rb
+++ spec/controllers/sounds_controller_spec.rb
@@ -22,4 +22,31 @@ RSpec.describe SoundsController, type: :controller do
       expect(response).to render_template(:show)
     end
   end
+
+  describe "GET to sounds" do
+    before do
+      @sound = Sound.new(title: "Sound")
+      @sound.file = "file.wav"
+      @sound.save!
+
+      @sound = Sound.new(title: "Sound2")
+      @sound.file = "file.wav"
+      @sound.save!
+
+      get :index
+    end
+
+    it "returns a 200 OK status code" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "loads the sounds" do
+      expect(assigns(:sounds).length).to eq(2)
+      expect(assigns(:sounds).first.title).to eq("Sound")
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template(:index)
+    end
+  end
 end
