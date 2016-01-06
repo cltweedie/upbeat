02bf89346880f05732ee0c8970ab2a97794ab19a
Chris Tweedie <cltweedie@gmail.com>
2016-01-06 12:13:15 +0000

    Move jquery to display hidden menus into haml view
    I had to move the javascript that set the event listener 'click' on the options into the view template - document.ready was only being triggered once, not allowing for choosing again without refreshing the page

--- app/assets/javascripts/sounds.js
+++ app/assets/javascripts/sounds.js
@@ -1,20 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(function() {
-
-  $('#instrument-option').click(function() {
-    console.log("clicked");
-    $('#instrument-select').toggle();
-  });
-
-  $('#category-option').click(function() {
-    console.log("clicked");
-    $('#category-select').toggle();
-  });
-
-  $('#tag-option').click(function() {
-    console.log("clicked");
-    $('#tag-select').toggle();
-  });
-
-});
