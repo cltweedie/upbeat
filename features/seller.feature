25a309f51c18599eddc16ae9194cdb8fc91d4e94
Chris Tweedie <cltweedie@gmail.com>
2016-01-09 14:25:17 +0000

    Add cucumber tests for registering as a seller

---
+++ features/seller.feature
@@ -0,0 +1,12 @@
+Feature: Sellers
+
+  @wip
+  Scenario: A producer registers as a seller
+    Given I have an account as a producer
+      And I'm signed in
+      And I'm on the sounds index page
+    When I choose to view my account settings
+    Then I will be taken to the account settings page
+    When I choose to register as a seller
+    Then I will see a flash notice "Thank you for registering as a seller."
+      And I will have an account as a seller
---
+++ features/step_definitions/seller_steps.rb
@@ -0,0 +1,15 @@
+When(/^I choose to view my account settings$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I will be taken to the account settings page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I choose to register as a seller$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I will have an account as a seller$/) do
+  pending # express the regexp above with the code you wish you had
+end
