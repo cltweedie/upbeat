1fc55c1e65ce16e7ca3376f4d3f416e1a8d701a1
Chris Tweedie <cltweedie@gmail.com>
2016-01-12 15:24:30 +0000

    Generate avatar uploader and add avatar to profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112105154) do
+ActiveRecord::Schema.define(version: 20160112152341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160112105154) do
     t.text     "bio"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "avatar"
   end
 
   create_table "purchases", force: :cascade do |t|
